7f3fdb34602c4dc980897d1ef229705db5da886d
pourya <pouryasalimi1@gmail.com>
2026-06-18 11:13:45 +0330

    add file upload endpoint

---
+++ photo-filter/src/main/java/personal/photo/filter/PhotoFilterApplication.java
@@ -0,0 +1,13 @@
+package personal.photo.filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoFilterApplication.class, args);
+	}
+
+}
